aa60240cf5ff8837d594ebc65c6ffc104ab7581d
snntaylan <sinan.taylan@outlook.com>
2019-06-28 17:39:57 +0200

    add project

--- app/config/routes.js
+++ app/config/routes.js
@@ -9,14 +9,14 @@ var HomeContainer = require("../containers/HomeContainer");
 var ForecastContainer = require("../containers/ForecastContainer");
 var CityDetailsContainer = require("../containers/CityDetailsContainer");
 
-// var routes = (
-// <Router history={hashHistory}>
-//   <Route path="/" component={Main}>
-//     <IndexRoute component={HomeContainer} />
-//     <Route path="/forecast/:city" component={ForecastContainer} />
-//     <Route path="/detail/:city" component={CityDetailsContainer} />
-//   </Route>
-// </Router>
-// );
+var routes = (
+  <Router history={hashHistory}>
+    <Route path="/" component={Main}>
+      <IndexRoute component={HomeContainer} />
+      <Route path="/forecast/:city" component={ForecastContainer} />
+      <Route path="/detail/:city" component={CityDetailsContainer} />
+    </Route>
+  </Router>
+);
 
 module.exports = routes;
